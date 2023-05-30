bb0b7d7bb6c3ae21dc39e5e7286053b20cb812c0
ymsk-sky <ymsk.sky.95@gmail.com>
2023-05-30 18:32:08 +0900

    検索結果から特定要素を取得する

--- js/content_script.js
+++ js/content_script.js
@@ -1,7 +1,19 @@
-$(function(){
-  var $button = $('<div class="switch-preview-button">●●●●●●●●●</div>');
-  $button.on("click", function(){
-    var url = location.href;
-  });
-  $("body").append($button);
-});
+// 要素一覧を取得
+const elements = document.getElementsByClassName("item-card l-card");
+console.log(elements.length);
+
+for (let i = 0; i < elements.length; i++) {
+  const element = elements[i];
+
+  // 条件に合う要素を非表示にする
+  if (element.getAttribute("data-product-brand") == "hanahanehomari") {
+    // タイトルを取得
+    const grandChildElement = element.querySelector(".item-card__title");
+    if (grandChildElement) {
+      const title = grandChildElement.innerText;
+      console.log("title:", title);
+    }
+    
+    element.parentNode.removeChild(element);
+  }
+}

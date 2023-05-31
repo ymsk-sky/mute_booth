5192052a03d162faf9fd994c1b44d081c348241d
ymsk-sky <ymsk.sky.95@gmail.com>
2023-05-31 22:28:59 +0900

    textareaの変更をstorageで記憶

---
+++ js/popup.js
@@ -0,0 +1,28 @@
+let memo;
+
+loadData();
+
+function loadData() {
+    chrome.storage.local.get(["chromememo"], function(obj) {
+        memo = obj.chromememo;
+
+        // データが無い場合の初期化
+        if(!memo) {
+            memo = [ {text: "", lastUpdate: new Date()} ];
+            chrome.storage.local.set({chromememo:memo}, function(){});
+            return;
+        }
+        $("#word_list").val(memo.text);
+    });
+}
+
+// 保存ボタンのアクション
+$("#save").on("click", function() {
+    memo = {
+        text: $("#word_list").val(),
+        lastUpdate: new Date()
+    };
+    chrome.storage.local.set({chromememo: memo}, function() {
+        alert("saved");
+    });
+});
